a3de69be1fb1f96b25677c6466c9b38ff1376dec
Swapna KY <swapnaky883@gmail.com>
2026-02-07 12:35:36 +0530

    updated

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", () => {
+        navbar.classList.toggle("scrolled", window.scrollY > 50);
+    });
+
+    const form = document.getElementById("contactForm");
+    const successMsg = document.getElementById("successMsg");
+
+    form.addEventListener("submit", function(e){
+        e.preventDefault();                 
+        successMsg.style.display = "block"; 
+    });
